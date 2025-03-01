4eb8c2c6311fb205832be42d8e5186b66ca99c8b
jacobocop <jacobomartincrespillo@gmail.com>
2025-03-01 17:01:15 +0100

    Corrigiendo referencias en los archivos

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,23 @@
-self.addEventListener("install", (event) => {
+const CACHE_NAME = "pictionary-cache-v1";
+const FILES_TO_CACHE = [
+    "./",
+    "./index.html",
+    "./manifest.json",
+    "./favicon.ico",
+    "./icon.png"
+];
+
+self.addEventListener("install", event => {
     event.waitUntil(
-        caches.open("pictionary-cache").then((cache) => {
-            return cache.addAll(["/", "/countdown.mp3"]);
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(FILES_TO_CACHE);
         })
     );
 });
 
-self.addEventListener("fetch", (event) => {
+self.addEventListener("fetch", event => {
     event.respondWith(
-        caches.match(event.request).then((response) => {
+        caches.match(event.request).then(response => {
             return response || fetch(event.request);
         })
     );
