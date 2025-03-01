c7c93a2d007fd9dc0497c2296e95ca1af62f2745
jacobocop <jacobomartincrespillo@gmail.com>
2025-03-01 15:11:48 +0100

    Primer commit del proyecto

---
+++ service-worker.js
@@ -0,0 +1,15 @@
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open("pictionary-cache").then((cache) => {
+            return cache.addAll(["/", "/countdown.mp3"]);
+        })
+    );
+});
+
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
